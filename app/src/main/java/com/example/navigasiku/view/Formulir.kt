4d4718987e7f9fca2cd92866fb28b545aa7069eb
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-06 17:20:51 +0700

    menambahkan kolom dengan padding dan alignment

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -44,5 +44,12 @@ fun FormIsian(
                     (containerColor = colorResource(id = R.color.teal_700))
             )
         }
-    ){}
+    ){
+            isiRuang ->
+        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {}
+
+        }
 }
